fca7e961a1fc7bb4167a379f3a7cf90809a2705b
ksaweryn <nirvomexvii@gmail.com>
2018-09-30 12:08:08 -0500

    Corrected

--- src/secondEditio/items/chap2/item2/NutritionFacts_BP.java
+++ src/secondEditio/items/chap2/item2/NutritionFacts_BP.java
@@ -15,10 +15,10 @@ public class NutritionFacts_BP {
         private final int serving;
 
         //Optional parameters
-        private int calories;
-        private int fat;
-        private int sodium;
-        private int carbohydrate;
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
 
         public Builder(int servingSize, int serving) {
             this.servingSize = servingSize;
@@ -58,19 +58,15 @@ public class NutritionFacts_BP {
         carbohydrate = builder.carbohydrate;
     }
 
-    public NutritionFacts_BP(int servingSize){
-        //Empty constructor
-    }
-
-
     public static void main(String[] args) {
-        NutritionFacts_BP cocaCola = new NutritionFacts_BP.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+        NutritionFacts_BP cocaCola = new NutritionFacts_BP.Builder(240, 8).calories(100).sodium(35).
+                carbohydrate(27).fat(300).build();
         System.out.println(cocaCola);
     }
 
     @Override
     public String toString() {
-        return "Servinsize: " + servingSize + ", serving: " + serving + ", calories: " + calories + ", fat: "
+        return "Serving size: " + servingSize + ", serving: " + serving + ", calories: " + calories + ", fat: "
                 + fat + ", sodium: " + sodium + ", carbohydrate: " + carbohydrate;
     }
 }
